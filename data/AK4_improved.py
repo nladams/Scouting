5ba70657ec130055a804cc92b1dd51d65428afb9
Nicholas Adams <157185381+nladams@users.noreply.github.com>
2024-03-20 18:22:19 -0400

    Update AK4_improved.py

--- data/AK4_improved.py
+++ data/AK4_improved.py
@@ -49,7 +49,7 @@ process.genJetSequence = cms.Sequence(
 
 # Create ParticleNet ntuple
 process.tree = cms.EDAnalyzer("AK4JetNtupleProducer",
-      isQCD = cms.untracked.bool( '/QCD_' in params.inputDataset ),
+      isQCD = cms.untracked.bool( '/QCD_' in params.inputTextFile),
       gen_jets = cms.InputTag( "genJetFlavourAssociation" ),
       pf_candidates = cms.InputTag( "hltScoutingPFPacker" ),
 )
